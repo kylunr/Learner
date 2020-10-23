0580cccd8b5c8445792f5f2477aa4aa2eac8846c
Kylun Robbins <kylun.robbins@gmail.com>
2020-10-22 17:12:25 -0700

    Updates

---
+++ precache-manifest.ef9287d109095749ee7d007447121b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0426051218c41da1f010abbeb1723be6",
+    "url": "/Learner/index.html"
+  },
+  {
+    "revision": "4a782ae377fdae826621",
+    "url": "/Learner/static/css/main.db5aa679.chunk.css"
+  },
+  {
+    "revision": "ba5e9132c1cf3ebdb36a",
+    "url": "/Learner/static/js/2.8d58e6b7.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/Learner/static/js/2.8d58e6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a782ae377fdae826621",
+    "url": "/Learner/static/js/main.efd59050.chunk.js"
+  },
+  {
+    "revision": "8d586b83079a3e7a0aa4",
+    "url": "/Learner/static/js/runtime-main.f62c29d3.js"
+  }
+]);
