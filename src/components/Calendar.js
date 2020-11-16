c42a8e77be43e859fe6943d56965b73d6e6d88ed
KylunR <kylun.robbins@gmail.com>
2020-11-15 20:47:57 -0800

    Saving before changes to calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,18 +1,61 @@
-import React, { useState, Component } from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import './Calendar.css';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import "./Calendar.css";
 
-class Calendar extends Component {
+// must manually import the stylesheets for each plugin                                                     
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 
-    render () {
+export default class Calendar extends React.Component {
+    calendarComponentRef = React.createRef();
+
+    state = {
+        calendarWeekends: true,
+        calendarEvents: [
+            // initial event data
+            { title: "Event Now", start: new Date() }
+        ]
+    };
+
+    render() {
         return (
-            <FullCalendar 
-                plugins={[ dayGridPlugin ]}
-                defaultView="dayGridMonth"
-            />
-        )
+            <div className="calendar">
+                <div className="calendar_header">
+                    <button onClick={this.toggleWeekends}>toggle weekends</button>&nbsp;
+                    <button onClick={this.gotoPast}>go to a date in the past</button>
+                    &nbsp; (also, click a date/time to add an event)
+                </div>
+                <div className="calendar_object">
+                    <FullCalendar
+                        defaultView="dayGridMonth"
+                        header={{
+                            left: "prev,next today",
+                            center: "title",
+                            right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+                        }}
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                        ref={this.calendarComponentRef}
+                        weekends={this.state.calendarWeekends}
+                        events={this.state.calendarEvents}
+                    />
+                </div>
+            </div>
+        );
     }
-}
 
-export default Calendar;
+    toggleWeekends = () => {
+        this.setState({
+            // update a property
+            calendarWeekends: !this.state.calendarWeekends
+        });
+    };
+
+    gotoPast = () => {
+        let calendarApi = this.calendarComponentRef.current.getApi();
+        calendarApi.gotoDate("2000-01-01"); // call a method on the Calendar object
+    };
+}   
